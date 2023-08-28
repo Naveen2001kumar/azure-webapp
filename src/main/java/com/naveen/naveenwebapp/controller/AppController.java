362b2384549eb74cbbb0e8e6a3c404a9f3bf076e
Naveen2001kumar <2016079@saec.ac.in>
2023-08-28 20:00:33 +0530

    adding output

---
+++ src/main/java/com/naveen/naveenwebapp/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.naveen.naveenwebapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+@GetMapping("/")
+    public String getContent()
+    {
+        return "<h1>hii iam naveen</h1>";
+    }
+}
