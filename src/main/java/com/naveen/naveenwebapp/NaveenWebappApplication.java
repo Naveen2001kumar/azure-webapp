0e5c8b5ec0d4bf53dd39e42b94b755524142735e
Naveen2001kumar <2016079@saec.ac.in>
2023-08-28 19:40:19 +0530

    first commit

---
+++ src/main/java/com/naveen/naveenwebapp/NaveenWebappApplication.java
@@ -0,0 +1,14 @@
+package com.naveen.naveenwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaveenWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaveenWebappApplication.class, args);
+
+	}
+
+}
