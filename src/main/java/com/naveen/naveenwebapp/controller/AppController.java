730a1ccab04b8a6d9de7f871ccd41ff139f18c9e
Naveen2001kumar <2016079@saec.ac.in>
2023-08-28 20:23:58 +0530

    requset initiated

--- src/main/java/com/naveen/naveenwebapp/controller/AppController.java
+++ src/main/java/com/naveen/naveenwebapp/controller/AppController.java
@@ -1,11 +1,12 @@
 package com.naveen.naveenwebapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
-    @GetMapping("get")
+    @RequestMapping("get")
     public String getContent()
     {
         return "<h1>hii iam naveen</h1>";
